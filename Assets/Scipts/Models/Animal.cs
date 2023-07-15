53307a66ea8787fb08f0b68311e026ce594810e4
Firat Arslan <firatarslan21@gmail.com>
2023-07-15 11:54:26 +0300

    commit
    Comments adding for Classes

--- Assets/Scipts/Models/Animal.cs
+++ Assets/Scipts/Models/Animal.cs
@@ -1,17 +1,19 @@
 using UnityEngine;
 using UnityEditor.Compilation;
 
-public abstract class Animal
+public abstract class Animal // MAIN CLASS
 {
+    // ENCAPSULATION Properties
     public int Age { get; set; }
     public string Name { get; set; }
     public float Speed { get; set; }
-
-    public virtual void Walk()
+ 
+    public virtual void Walk()// POLYMORPHISM
     {
         Debug.Log("im walking");
     }
 
+    // ABSTRACTION
     public abstract void GiveVoice();
 
 }
