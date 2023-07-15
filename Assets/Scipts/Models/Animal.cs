4970b5e5abd017f3f6992b497d068458f1922916
Firat Arslan <firatarslan21@gmail.com>
2023-07-15 11:04:13 +0300

    Initial commit

---
+++ Assets/Scipts/Models/Animal.cs
@@ -0,0 +1,8 @@
+public abstract class Animal
+{
+    public int Age { get; set; }
+    public string Name { get; set; }
+
+    public abstract void GiveVoice();
+
+}
---
+++ Assets/Scipts/Models/Dog.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dog : Animal
+{
+    public override void GiveVoice()
+    {
+        Debug.Log("havv");
+    }
+}
